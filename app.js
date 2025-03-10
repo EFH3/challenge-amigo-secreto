184202a894c47479452e70afebc5ac57f4bcff66
Ever Fierro <everhdz3000@gmail.com>
2025-03-10 11:58:00 -0700

    Se agrega nueva funcionalidad para agregar los nombres de los amigos a la lista.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value.trim(); //trim elimina los espacios en blanco iniciales y finales
+
+    if (nombreAmigo === '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        if (validarInput(nombreAmigo)) {
+            amigos.push(nombreAmigo);
+            limpiarCaja();
+            console.log(amigos);
+        }
+    }
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function validarInput(nombreAmigo) {
+    let letras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/; // Solo letras y espacios
+    let inputValido = false;
+
+    if (letras.test(nombreAmigo)) {
+        document.getElementById("mensaje").textContent = "✅ Entrada válida";
+        inputValido = true;
+    } else {
+        document.getElementById("mensaje").textContent = "❌ Solo se permiten letras";
+    }
+    return inputValido;
+}
